9a2843518c4a7147347173bd5838d2820130316a
adamvinueza <adamvinueza@pm.me>
2020-01-25 19:33:44 -0700

    additional tests

--- localTime/localTime.js
+++ localTime/localTime.js
@@ -2,6 +2,6 @@ const moment = require('moment-timezone');
 
 // Returns the local time as an ISO 8601 string in the specified time zone.
 const localTime = async tzName => {
-  return '';
+  return new moment().format();
 };
 module.exports = localTime;
--- test/localTimeTest.js
+++ test/localTimeTest.js
@@ -21,4 +21,71 @@ describe(`GET ${endpoint}`, () => {
         done();
       });
   });
+  it('Should return the current time as UTC with no query string', done => {
+    chai.request(server)
+      .get(endpoint)
+      .end((err, res) => {
+        expect(err).to.be.null;
+        res.should.have.status(200);
+        // const expected = ? 
+        const actual = new moment(res.text, moment.ISO_8601, PARSE_STRICT);
+        actual.isValid.should.equal(true);
+        // compare actual and expected here
+        done()
+    });
+  });
+  it('Should return 7 am UTC as midnight Denver time (UTC -7h)', done => {
+    chai.request(server)
+      .get(endpoint)
+      .query({}) // what goes into the query part?
+      .end((err, res) => {
+        expect(err).to.be.null;
+        res.should.have.status(200);
+        // const expected = ? 
+        const actual = new moment(res.text, moment.ISO_8601, PARSE_STRICT);
+        actual.isValid.should.equal(true);
+        // compare actual and expected here
+        done()
+    });
+  });
+  it('Should return 7 am UTC as 1 am Chicago time (UTC -6h)', done => {
+    chai.request(server)
+      .get(endpoint)
+      .query({}) // what goes into the query part?
+      .end((err, res) => {
+        expect(err).to.be.null;
+        res.should.have.status(200);
+        // const expected = ? 
+        const actual = new moment(res.text, moment.ISO_8601, PARSE_STRICT);
+        actual.isValid.should.equal(true);
+        // compare actual and expected here
+        done()
+    });
+  });
+  it('Should return 7 am UTC as 9 am Basel time (UTC +1h)', done => {
+    chai.request(server)
+      .get(endpoint)
+      .query({}) // what goes into the query part?
+      .end((err, res) => {
+        expect(err).to.be.null;
+        res.should.have.status(200);
+        // const expected = ? 
+        const actual = new moment(res.text, moment.ISO_8601, PARSE_STRICT);
+        actual.isValid.should.equal(true);
+        // compare actual and expected here
+        done()
+    });
+  });
+  it('Should return a 400 response when the time zone specified is invalid', done => {
+    chai.request(server)
+      .get(endpoint)
+      .query({}) // what goes into the query part?
+      .end((err, res) => {
+        expect(err).to.be.null;
+        res.should.have.status(400);
+        // const expected = ? 
+        // const actual = ?
+        done()
+      });
+  });
 });
